d870896a7a4d64cf95b6172db2a6afdafc4132df
ArtificialImmunity <11080513@brookes.ac.uk>
2015-04-09 14:26:30 +0100

    Create resetdb.sh
    Quick script to reset the database. Good to use when testing and need a fresh database to collect un-tainted results

---
+++ resetdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d /var/lib/mysql/snort ]; then
+
+		for mysqlCommand in "drop database snort;" "drop database archive"
+        do
+                    echo $mysqlCommand >> mysqlCommands
+        done
+
+		mysql -u root -p --password='password'<mysqlCommands
+		rm mysqlCommands
+
+
+        for mysqlCommand in "create database snort;" "create database archive;" "grant usage on snort.* to snort@localhost;" "grant usage on archive.* to snort@localhost;" "set password for snort@localhost=PASSWORD('password');" "grant all privileges on snort.* to snort@localhost;" "grant all privileges on archive.* to snort@localhost;" "flush privileges;" "use snort;" "source /usr/src/create_mysql;"
+        do
+                    echo $mysqlCommand >> mysqlCommands
+        done
+#       echo "---------------------------------------------------"
+#       echo "  About to change MySQL, enter MySQL credentials."
+#       echo "---------------------------------------------------"
+        mysql -u root -p --password='password'<mysqlCommands #simple pass for testing purposes only
+        rm mysqlCommands
+        rm /var/log/snort/*
+#        rm /var/log/barnyard2/*
+
+fi;
