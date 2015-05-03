18bf9358abd344de2f28517fb557bd766b12fa64
Jordan Bruce <11080513@brookes.ac.uk>
2015-05-03 14:03:24 +0100

    updated script

--- scripts/deployAgent.sh
+++ scripts/deployAgent.sh
@@ -26,16 +26,16 @@ AISDEPLOYPATH="/usr/local/src/ais/deploy/ais/"
 mkdir /usr/local/src/ais/deploy $AISDEPLOYPATH $AISDEPLOYPATH/agents $AISDEPLOYPATH/scripts $AISDEPLOYPATH/tablesmysql
 
 #copy agent files to deploy folder
-cp  $AISFILEPATH/agents/{__init__.py,agentlib.py,netagent.py,sysagent.py} $AISDEPLOYPATH/agents/
+cp  $AISFILEPATH/agents/{__init__.py, agentlib.py, netagent.py, sysagent.py} $AISDEPLOYPATH/agents/
 
 #copy scripts to deploy folder
-cp $AISFILEPATH/scripts/{collectAndFlush.sh,startbarnyard.sh} $AISDEPLOYPATH/scripts
+cp $AISFILEPATH/scripts/{agentInstall.sh, collectAndFlush.sh, startbarnyard.sh} $AISDEPLOYPATH/scripts
 
 #copy tables structs to deploy folder
-cp $AISFILEPATH/tablesmysql/{create_mysql_banlist_agent,create_mysql_syslog} $AISDEPLOYPATH/tablesmysql
+cp $AISFILEPATH/tablesmysql/{create_mysql_banlist_agent, create_mysql_syslog} $AISDEPLOYPATH/tablesmysql
 
 #copy over scripts
 scp -r $AISDEPLOYPATH ubuntu@$1:~/
 ssh -t ubuntu@$1 "sudo mv $HOME/ais /usr/local/src/; sudo . $AISFILEPATH/scripts/agentInstall.sh"
 rm -r $AISFILEPATH/deploy
-cat $1 >> $AISFILEPATH/agents/allAgentIPs
+cat "$1" >> $AISFILEPATH/agents/allAgentIPs
