5093e45d681a31326217d0847d75a578974743e2
Jordan Bruce <11080513@brookes.ac.uk>
2015-05-03 15:26:59 +0100

    now bans inputted ip from args

--- scripts/banGlobalIP.sh
+++ scripts/banGlobalIP.sh
@@ -5,7 +5,6 @@
 if [ $# -eq 1 ]; then #if  number of args = 1, i.e. 1 ip address
     while read ip
     do
-            ssh ubuntu@ip 'iptables -A INPUT -s $1 -j DROP'
-            ssh ubuntu@ip 'iptables -A OUTPUT -d $1 -j DROP'
+        ssh ubuntu@$ip "/usr/bin/sudo bash -c 'iptables -A INPUT -s $1 -j DROP; iptables -A OUTPUT -d $1 -j DROP'"
     done </usr/local/src/ais/agents/allAgentIPs
 fi;
